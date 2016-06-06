62e8fe8606a51f0c08b6453d95d406db3e2ab211
iuliasarb <sarb_iulia@yahoo.com>
2016-06-06 12:14:58 +0200

    added save button

---
+++ save.js
@@ -0,0 +1,22 @@
+var saveButton = document.getElementById('save');
+
+saveButton.addEventListener('click', saveImage);
+
+/*
+function saveImage() {
+	var data = canvas.toDataURL();
+	window.open(data, '_blank', 'location=0, menubar=0'); // window open() Method --> google search
+}
+*/
+
+function saveImage(){
+	var canvas = document.getElementById('canvas');
+	var image = canvas.toDataURL();
+
+	var aLink = document.createElement('a');
+	var evt = document.createEvent("HTMLEvents");
+	evt.initEvent('click');
+	aLink.download = 'image.png';
+	aLink.href = image;
+	aLink.dispatchEvent(evt);
+}
